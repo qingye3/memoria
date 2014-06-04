4a464c782e1070d96870ce02031dc0b9494549f9
qingye3 <qingye3@illinois.edu>
2014-06-04 23:14:11 +0800

    changed words

--- words.py
+++ words.py
@@ -1,11 +1,6 @@
-class Definition:
-    def __init__(self, text = "", part_of_speech = "n"):
-        #part of speech
-        self.pos = part_of_speech 
-        self.text = text 
-
 class Word:
     def __init__(self):
         self.word = ""
-        self.definitions = [] 
+        self.definitions = "" 
         self.proficiency = 0.0
+
