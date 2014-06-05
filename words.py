4f346619b20f4771febfb5cb3ea73a66e9fc5cf8
qingye3 <qingye3@illinois.edu>
2014-06-05 14:20:18 +0800

    updated words.py, not tested

--- words.py
+++ words.py
@@ -1,6 +1,39 @@
+# -*- coding: utf-8 -*-
+from pystardict import Dictionary
 class Word:
-    def __init__(self):
-        self.word = ""
-        self.definitions = "" 
+    def __init__(self, word = "", definition = ""):
+        self.word = word 
+        self.definition = definition 
         self.proficiency = 0.0
 
+class Wordlist:
+    def __init__(self):
+        self.words = []
+
+    def delete(self):
+        del self.words
+        self.words = []
+
+    def append_words(self, string):
+        for line in string:
+            line = line.strip()
+            if line.startswith("#"):
+                continue
+            self.words += [Word(word.strip()) for word in line.split(",")]
+
+    def import_words(self, string):
+        self.delete()
+        self.append_words(string)
+
+    def import_definitions(self, dictionary):
+        the_dict = Dictionary(dictionary) 
+        for word in self.words:
+            if word.definition == "":
+                try:
+                    word.definition = the_dict[word]
+                except KeyError:
+                    pass
+                    
+    def counts(self):
+        return len(self.words)
+        
